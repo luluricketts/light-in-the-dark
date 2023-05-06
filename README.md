410c7dc68c3b7f0a021b0256f237eca41c0a9307
Lulu <lulu.ricketts@gmail.com>
2023-05-06 18:35:38 -0400

    enlightengan

--- README.md
+++
@@ -1,3 +0,0 @@
-# Illumination Recory
-
-16-726 Learning for Image Synthesis Final Project
--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-# init
-
-from .model_utils import conv, up_conv, ResnetBlock
-from .base_generator import CycleGenerator
-from .patch_discriminator import PatchDiscriminator
-from .cycleGAN import CycleGAN
-from .dataloader import get_data_loader
-
